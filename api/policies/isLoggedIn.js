833e5b00b3ad771c837271f38c63d05fe86b61de
khanrezwan <rezwanislam@gmail.com>
2014-12-06 13:02:24 +0600

    updated policy isLoggedIn and .sailrc

--- api/policies/isLoggedIn.js
+++ api/policies/isLoggedIn.js
@@ -1,10 +1,14 @@
 module.exports = function isLoggedIn (req, res, next) {
 //if no user logged in then redirect to login page
 
-  if ( !req.user ) {
+  if ( req.user ) {
+   verbose(req.user.name+" Logged in");
+    next();
+  }
+  else
+  {
     return res.redirect('/');
   }
 
 
-  next();
 };
